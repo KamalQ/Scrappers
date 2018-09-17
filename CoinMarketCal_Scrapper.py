7889ce58f686a50f3d5ad77fded25bb2d45a8ee6
metalwii <kamalmqureshi@gmail.com>
2018-09-17 17:38:06 -0400

    added symbol

--- CoinMarketCal_Scrapper.py
+++ CoinMarketCal_Scrapper.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import datetime, time
+import datetime as dt, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
@@ -20,24 +20,28 @@ class fetch_coinMarketCal:
         self.chromedriver = "/usr/local/bin/chromedriver"
         self.driver = webdriver.Chrome(chrome_options=self.options, executable_path=self.chromedriver)
 
-    def upcoming_event(self):
+
+    # past events == https://coinmarketcal.com/pastevents
+    def upcoming_events(self):
         self.driver.get("https://coinmarketcal.com")
         page_count = 1
         list_entries = []
-        time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        time_stamp = dt.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
         while True:
-            page_count += 1
             for entry in self.driver.find_elements_by_class_name('card__body'):
                 coin_name = entry.find_element_by_xpath('.//h5[@class="card__coins"]/a').text
                 date = entry.find_element_by_xpath('.//a/h5[@class="card__date"]').text
                 event_name = entry.find_element_by_xpath('.//a/h5[@class="card__title"]').text
                 additional_info = entry.find_element_by_xpath('.//div/p[@class="card__description"]').text
-                list_entries.append({'name': coin_name, 'date': date, 'event_name': event_name, 'additional_info': additional_info})
+                #link = entry.find_element_by_xpath('.//h5[@class="card__coins"]/a').text
+                symbol = coin_name.split('(', 1)[1].split(')')[0]
+                list_entries.append({'symbol': symbol, 'name': coin_name, 'date': date, 'event_name': event_name, 'additional_info': additional_info})
             try:
-                print(time_stamp, 'Finished scraping data from page #', (page_count-1))
+                print(time_stamp, 'Finished scraping data from page #', (page_count))
                 element = self.driver.find_element_by_class_name("pagination")
                 self.driver.execute_script("arguments[0].scrollIntoView();", element)
+                page_count += 1
                 self.driver.find_element_by_link_text(str(page_count)).click()
             except NoSuchElementException:
                 break
@@ -47,10 +51,10 @@ class fetch_coinMarketCal:
         df_entries = pd.DataFrame.from_dict(list_entries)
         print(df_entries)
         df_entries.to_csv('CoinMarketCal_Upcoming_Scrapped_Data.csv')
-        # Todo parse the coin Symbol and add into new column, sort DF via symbol
+        #Todo add column for hyperlink, sort by symbol in df
         self.driver.quit()
 
 
 if __name__ == '__main__':
     scrapper = fetch_coinMarketCal()
-    scrapper.upcoming_event()
+    scrapper.upcoming_events()
