0bfaa7c9c9fde5785f613fde291f7ab244eb22de
metalwii <kamalmqureshi@gmail.com>
2018-09-17 20:48:39 -0400

    Scrapes for both Future and Past events, added column for hyperlink to event

--- CoinMarketCal_Scrapper.py
+++ CoinMarketCal_Scrapper.py
@@ -20,10 +20,8 @@ class fetch_coinMarketCal:
         self.chromedriver = "/usr/local/bin/chromedriver"
         self.driver = webdriver.Chrome(chrome_options=self.options, executable_path=self.chromedriver)
 
-
-    # past events == https://coinmarketcal.com/pastevents
-    def upcoming_events(self):
-        self.driver.get("https://coinmarketcal.com")
+    def events(self, event_link, file_name):
+        self.driver.get(event_link)
         page_count = 1
         list_entries = []
         time_stamp = dt.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
@@ -34,11 +32,12 @@ class fetch_coinMarketCal:
                 date = entry.find_element_by_xpath('.//a/h5[@class="card__date"]').text
                 event_name = entry.find_element_by_xpath('.//a/h5[@class="card__title"]').text
                 additional_info = entry.find_element_by_xpath('.//div/p[@class="card__description"]').text
-                #link = entry.find_element_by_xpath('.//h5[@class="card__coins"]/a').text
+                link = entry.find_elements_by_class_name('link-detail')[1].get_attribute('href')
                 symbol = coin_name.split('(', 1)[1].split(')')[0]
-                list_entries.append({'symbol': symbol, 'name': coin_name, 'date': date, 'event_name': event_name, 'additional_info': additional_info})
+                list_entries.append({'symbol': symbol, 'name': coin_name, 'date': date, 'event_name': event_name,
+                                     'additional_info': additional_info, 'link': link})
             try:
-                print(time_stamp, 'Finished scraping data from page #', (page_count))
+                print(time_stamp, 'Finished scraping data from page #', page_count)
                 element = self.driver.find_element_by_class_name("pagination")
                 self.driver.execute_script("arguments[0].scrollIntoView();", element)
                 page_count += 1
@@ -50,11 +49,11 @@ class fetch_coinMarketCal:
 
         df_entries = pd.DataFrame.from_dict(list_entries)
         print(df_entries)
-        df_entries.to_csv('CoinMarketCal_Upcoming_Scrapped_Data.csv')
-        #Todo add column for hyperlink, sort by symbol in df
+        df_entries.to_csv(file_name)
         self.driver.quit()
 
 
 if __name__ == '__main__':
     scrapper = fetch_coinMarketCal()
-    scrapper.upcoming_events()
+    scrapper.events('https://coinmarketcal.com', 'CoinMarketCal_Scrapped_Data.csv')
+    scrapper.events('https://coinmarketcal.com/pastevent', 'CoinMarketCal_Past_Scrapped_Data.csv')
