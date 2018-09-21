c94162501152e2a14740a1aa6d0da86e3cf3b4ae
metalwii <kamalmqureshi@gmail.com>
2018-09-21 17:36:21 -0400

    now inputs start date into Calendar date picker, Also closes sign-up prompt when opening the site

--- Historical_data/Crypto_Historical_Data_Miner.py
+++ Historical_data/Crypto_Historical_Data_Miner.py
@@ -23,23 +23,34 @@ class fetch_crypto_hist_data:
         self.chromedriver = "/usr/local/bin/chromedriver"
         self.driver = webdriver.Chrome(chrome_options=self.options, executable_path=self.chromedriver)
 
-    def fetch_symbol_name(self,symbol):
+    def fetch_symbol_name(self):
         read_dictionary = np.load('../CoinMarketCap/List_of_crypto_symbols.npy').item()
-        symbol_name = read_dictionary[symbol]
+        symbol_name = read_dictionary[self]
         return symbol_name
 
-    def get_to_page(self, symbol):
-        symbol_name = fetch_crypto_hist_data.fetch_symbol_name(self,symbol)
-        symbol = symbol.lower()
+    def close_sign_in_prompt(self):
+        self.driver.find_element_by_css_selector('i.popupCloseIcon.largeBannerCloser').click()
+        fetch_crypto_hist_data.get_to_page(self)
+
+    def get_to_page(self):
+        symbol_name = fetch_crypto_hist_data.fetch_symbol_name(self.symbol)
+        symbol = self.symbol.lower()
         symbol_name = symbol_name.lower()
         self.driver.get('https://ca.investing.com/crypto/{}/{}-btc-historical-data'.format(symbol_name, symbol))
-        #todo- clear startdate before inputinng it
         start_date = '01/01/2016'
-        # self.driver.find_elements_by_id('p_chan_links_lnk')[10].click()
-        # self.driver.find_element_by_partial_link_text('Look-up Classes to Add').click()
-        # self.driver.quit()
+        try:
+            self.driver.find_elements_by_class_name('datePickerIcon')[1].click()
+            self.driver.find_element_by_id('startDate').clear()
+            self.driver.find_element_by_id('startDate').send_keys(start_date)
+            self.driver.find_element_by_id('applyBtn').click()
+        except:
+            fetch_crypto_hist_data.close_sign_in_prompt(self)
+
+    def fetch_data(self,symbol):
+        self.symbol = symbol
+        fetch_crypto_hist_data.get_to_page(self)
 
 
 if __name__ == '__main__':
     scrapper = fetch_crypto_hist_data()
-    scrapper.get_to_page('TRX')
+    scrapper.fetch_data('TRX')
